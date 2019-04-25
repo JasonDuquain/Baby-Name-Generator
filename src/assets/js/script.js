54d563c5645e465aae1c6b83a4d89b37b221c151
jasonduquain <jduquain@gmail.com>
2019-04-24 23:02:56 -0400

    animate-and-style-main-page_user-story8: completed

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,15 +1,42 @@
+/****** Main page animation on first load  *******/
 
-TweenMax.to('.header__title', 3, { scrambleText: 
-    { 
-        text: 'Baby Name Generator', 
-        chars: "lowerCase", 
-        speed: 0.1
-    }
-});
+// this check ensures the main page animation only happens once - it will only happen again if the user closes their brower and re-opens app
+if (!sessionStorage.getItem('stopanimate')) {
+   
+    let mainGroup = Array.from(document.querySelectorAll('.main__group, .main__submit-wrap'));
+    
+    let introText = new SplitText('.intro__para', {type: "lines"});
 
+    let tl = new TimelineMax();
+    tl.to('.header__title', 3, { scrambleText: 
+        { 
+            text: 'Baby Name Generator', 
+            chars: "lowerCase", 
+            speed: 0.1
+        }
+    });
+    tl.staggerFrom(introText.lines, .7, {
+        opacity:0, 
+        rotationX:-120, 
+        force3D:true, 
+        transformOrigin:"top center -150",
+        ease: Power2.easeOut
+    }, 1);
+    tl.staggerFrom(mainGroup, 1, { 
+        opacity: 0,
+        y: -100,
+        ease: Back.easeOut,
+        onComplete: function() {
+            document.body.classList.add('stopanimate')
+            sessionStorage.setItem('stopanimate', 'stop')
+        }
+    }, .2, "+=.2");
+     
+}
+
+
+/***** Animate icon colors in input element focus  ******/
 let inputs = document.querySelectorAll('.main__select, .main__input');
-
-
 
 inputs.forEach(el => {
     el.addEventListener('focus', focusChange)
@@ -29,3 +56,8 @@ function blurChange(e) {
     let icon = e.target.previousElementSibling;
     icon.classList.remove('focus');
 }
+
+
+/***** Update Copyright Year to always be current  ******/
+let year = document.querySelector('.footer__year');
+year.textContent = new Date().getFullYear();
